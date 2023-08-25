be4823543038b460f7e0774d0a079de3c960ea2c
Patrick Coxall <mr.coxall@mths.ca>
2023-08-25 09:27:31 -0400

    code init

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# TEJ2O ⇢ Unit #2-03
+# TEJ2O ⇢ Unit #2-03B
 
-[![Mr Coxall's Super Linter](https://github.com/TEJ2O-Templates/TEJ2O-Unit2-03/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/TEJ2O-Templates/TEJ2O-Unit2-03/actions)
+[![Mr Coxall's Super Linter](https://github.com/TEJ2O-Templates/TEJ2O-Unit2-03B/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/TEJ2O-Templates/TEJ2O-Unit2-03B/actions)
 
 [![CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-blue.svg)](./LICENSE)
 
--- main.ts
+++ main.ts
@@ -8,38 +8,17 @@
 basic.clearScreen()
 basic.pause(1000)
 
-// addition
-basic.showString('40 + 2 = ' + (40 + 2).toString())
+// text
+basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
+
+// perimeter
+basic.showString('The perimeter would be: ' + (2 * (5 + 3)).toString() + ' cm.')
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
 
-// subtraction
-basic.showString('45 - 3 = ' + (45 - 3).toString())
-basic.showIcon(IconNames.Happy)
-basic.pause(100)
-basic.clearScreen()
-
-// multiply
-basic.showString('2 + 8 x 5 = ' + (2 + 8 * 5).toString())
-basic.showIcon(IconNames.Happy)
-basic.pause(100)
-basic.clearScreen()
-
-// divide
-basic.showString('(80 / 2) + 2 = ' + (80 / 2 + 2).toString())
-basic.showIcon(IconNames.Happy)
-basic.pause(100)
-basic.clearScreen()
-
-// incorrect way to do exponents
-basic.showString('10 + 2^5 = ' + (10 + 2 ^ 5).toString())
-basic.showIcon(IconNames.Sad)
-basic.pause(100)
-basic.clearScreen()
-
-// exponent
-basic.showString('10 + 2^5 = ' + (10 + 2 ** 5).toString())
+// area
+basic.showString('The area would be: ' + (5 * 3).toString() + ' cm^2.')
 basic.showIcon(IconNames.Happy)
 basic.pause(100)
 basic.clearScreen()
